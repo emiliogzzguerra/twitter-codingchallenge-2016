6bbc6110502d96fdfa6f0d48bbca84bc864d5d23
Emilio Gonzalez <emiliogzz24@gmail.com>
2016-10-05 18:00:03 -0500

    First commit

---
+++ FirstProblem/FirstProblem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstProblem
+//
+//  Created by Emilio González  on 10/5/16.
+//  Copyright © 2016 Emilio González . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
